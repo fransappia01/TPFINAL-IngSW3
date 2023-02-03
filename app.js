b7479bca97a3b69e480be0ca3fc5b52aacb8ff02
fran <fransappia01@gmail.com>
2023-02-02 22:53:54 -0300

    solucion cors

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ require('dotenv').config()
 
 const app = express();
 
-var corsOptions = {
-  origin: ["http://localhost:8081","http://localhost:5173","https://deluxe-dust-production-isw3.up.railway.app/"]
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
