458bdf3801f84c2b09bd5f354e70ba75a6959d49
fran <fransappia01@gmail.com>
2023-02-16 12:56:34 -0300

    unit test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     verbose: true,
-    setupFiles: ["dotenv/config"]
+    setupFiles: ["dotenv/config"],
+    collectCoverage: true,
+    coverageReporters: ["json", "html"],
+    reporters: [
+        'default',
+        ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }],
+    ],
 };
